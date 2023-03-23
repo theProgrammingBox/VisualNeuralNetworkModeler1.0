d963648448e8d77bc141ade8173e30c6764d6bfa
theProgrammingBox <justinboxgea@gmail.com>
2023-03-23 15:59:22 -0700

    working low res curve line

--- VisualNeuralNetworkModeler1.0/DirectedConnection.h
+++ VisualNeuralNetworkModeler1.0/DirectedConnection.h
@@ -41,13 +41,9 @@ public:
 			closestPointToMouse = startNodePosition + delta * t / squaredLength;
 		}
 		
-		//pge->DrawLine(startNodePosition, endNodePosition, matrixNode->GetColor());
-		//DrawThickDirectedLine(startNodePosition, endNodePosition, matrixNode->GetColor(), 3);
-		DrawBezierCurve(startNodePosition, olc::vf2d(-1, 0), endNodePosition, olc::vf2d(1, 0), matrixNode->GetColor(), 3);
+		DrawBezierCurve(startNodePosition, olc::vf2d(-1, 0), endNodePosition, olc::vf2d(1, 0), matrixNode->GetColor(), 1);
 		if ((point - closestPointToMouse).mag2() <= 64)
-			//pge->DrawLine(closestPointToMouse, matrixNodePosition, matrixNode->GetColor(), 0x44444444);
-			//DrawThickDirectedLine(matrixNodePosition, closestPointToMouse, matrixNode->GetColor(), 3);
-			DrawBezierCurve(matrixNodePosition, olc::vf2d(0, -1), closestPointToMouse, olc::vf2d(0, -1), matrixNode->GetColor(), 3);
+			DrawBezierCurve(matrixNodePosition, olc::vf2d(0, -1), closestPointToMouse, olc::vf2d(0, 1), matrixNode->GetColor(), 1);
 	}
 
 private:
@@ -57,35 +53,22 @@ private:
 	TextBox* endNode;
 	TextBox* matrixNode;
 
-	/*void DrawThickDirectedLine(const olc::vi2d& p1, const olc::vi2d& p2, const olc::Pixel& color, uint8_t thickness) {
+	void DrawThickDirectedLine(const olc::vf2d& p1, const olc::vf2d& p2, const olc::Pixel& color, uint8_t radius = 1) {
 		olc::vf2d delta = p2 - p1;
-		float length = delta.mag();
-		if (length == 0) return;
-		olc::vf2d normDelta = delta / length * thickness * 0.5f;
-		olc::vf2d perpDelta = normDelta.perp();
-		olc::vf2d shift = -normDelta * 0.866025403784f * 10;
+		float sqrLength = delta.mag2();
+		if (sqrLength == 0) return;
+		olc::vf2d perpDelta = delta.perp() / std::sqrt(sqrLength) * radius;
 		olc::vf2d p1m = p1 - perpDelta;
-		olc::vf2d p2p = p2 + perpDelta + shift;
-		olc::vf2d p2m = p2 - perpDelta + shift;
+		olc::vf2d p2p = p2 + perpDelta;
 		pge->FillTriangle(p1 + perpDelta, p1m, p2p, color);
-		pge->FillTriangle(p2m, p1m, p2p, color);
+		pge->FillTriangle(p2 - perpDelta, p1m, p2p, color);
+	}
 
-		olc::vf2d tri1 = p2 + perpDelta * 0.5f * 10;
-		olc::vf2d tri2 = p2 - perpDelta * 0.5f * 10;
-		pge->FillTriangle(p2, tri1 + shift, tri2 + shift, color);
-	}*/
-
-    void DrawBezierCurve(olc::vf2d p0, olc::vf2d normal0, olc::vf2d p3, olc::vf2d normal3, olc::Pixel color, uint8_t thickness = 1) {
+    void DrawBezierCurve(olc::vf2d p0, olc::vf2d normal0, olc::vf2d p3, olc::vf2d normal3, olc::Pixel color, uint8_t radius = 1) {
 		olc::vf2d p1 = p0 + 128 * normal0;
-		olc::vf2d p2 = p3 - 128 * normal3;
-        olc::Pixel underColor = color * 0.7f;
-        olc::Pixel drawingColor;
-
-        const olc::vf2d shift = olc::vf2d(0, thickness * 0.5f);
-        olc::vf2d pastPointTop = p0 + shift;
-        olc::vf2d pastPointBottom = p0 - shift;
-
-        olc::vf2d centerPoint, newPointTop, newPointBottom;
+		olc::vf2d p2 = p3 + 128 * normal3;
+		olc::vf2d pastPoint = p0;
+        olc::vf2d newPoint;
 
         float t = 0;
         float e1, e2, e3, c0, c1, c2, c3;
@@ -100,59 +83,9 @@ private:
             c2 = e2 - e3;
             c3 = 0.3333f * e3;
 
-            centerPoint = p0 * c0 + p1 * c1 + p2 * c2 + p3 * c3;
-            newPointTop = centerPoint + shift;
-            newPointBottom = centerPoint - shift;
-
-            drawingColor = color;
-            if (newPointTop.x < pastPointTop.x)
-                drawingColor = underColor;
-
-            pge->FillTriangle(pastPointTop, pastPointBottom, newPointTop, drawingColor);
-            pge->FillTriangle(newPointBottom, pastPointBottom, newPointTop, drawingColor);
-
-            pastPointTop = newPointTop;
-            pastPointBottom = newPointBottom;
+            newPoint = p0 * c0 + p1 * c1 + p2 * c2 + p3 * c3;
+			DrawThickDirectedLine(pastPoint, newPoint, color, radius);
+			pastPoint = newPoint;
         }
     }
-
-    /*void DrawAnimatedArrowBezierCurve(olc::vf2d p0, olc::vf2d p3, olc::Pixel color, uint8_t thickness = 1) {
-        olc::vf2d p1 = p0 + olc::vf2d(128, 0);
-        olc::vf2d p2 = p3 - olc::vf2d(128, 0);
-        olc::Pixel underColor = color * 0.7f;
-        olc::Pixel drawingColor;
-
-        const olc::vf2d shift = olc::vf2d(0, thickness * 0.5f);
-        olc::vf2d pastPointTop = p0 + shift;
-        olc::vf2d pastPointBottom = p0 - shift;
-
-
-        float t = 0;// animationTimer;
-        float e1, e2, e3, c0, c1, c2, c3;
-        olc::vf2d centerPoint;
-        for (uint8_t i = 0; i < 96; ++i) {
-            t += 0.01f;
-            e1 = 3 * t;
-            e2 = e1 * t;
-            e3 = e2 * t;
-
-            c0 = 1 - 0.3333f * e3 + e2 - e1;
-            c1 = e3 - 2 * e2 + e1;
-            c2 = e2 - e3;
-            c3 = 0.3333f * e3;
-
-            centerPoint = p0 * c0 + p1 * c1 + p2 * c2 + p3 * c3;
-
-            drawingColor = color;
-            if (centerPoint.x < pastPointTop.x)
-                drawingColor = underColor;
-
-            if ((i & 3) == 2) {
-                FillTriangle(pastPointTop, pastPointBottom, centerPoint, drawingColor);
-            }
-
-            pastPointTop = centerPoint + shift;
-            pastPointBottom = centerPoint - shift;
-        }
-    }*/
 };
